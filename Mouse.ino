deb6e6ff521aca4b2601dd738429fa474dc84c2e
Trevor Satori <80570915+TrevorSatori@users.noreply.github.com>
2022-04-21 16:32:26 -0500

    Mouse Driver Implemented

--- Mouse.ino
+++ Mouse.ino
@@ -1,37 +1,122 @@
+#include <hidboot.h>
+#include <usbhub.h>
 #include <Mouse.h>
+#include <Wire.h>
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
 #include <SPI.h>
+USB     Usb;
+USBHub     Hub(&Usb);
+HIDBoot < USB_HID_PROTOCOL_KEYBOARD | USB_HID_PROTOCOL_MOUSE > HidComposite(&Usb);
+HIDBoot<USB_HID_PROTOCOL_MOUSE>    HidMouse(&Usb);
 
 // Signed char can be between -128 to 127
 int delta[2];
-int deltaX;
-int deltaY;
+int negMax = -127;
+int posMax = 127;
 
-int negMax = -125;
-int posMax = 125;
+// Mouse
+int lmb = 0;
+int rmb = 0;
+int mmb = 0;
 
-void setup() {
-  Serial.begin(9600);
+
+class MouseRptParser : public MouseReportParser
+{
+  protected:
+    void OnMouseMove(MOUSEINFO *mi);
+    void OnLeftButtonUp(MOUSEINFO *mi);
+    void OnLeftButtonDown(MOUSEINFO *mi);
+    void OnRightButtonUp(MOUSEINFO *mi);
+    void OnRightButtonDown(MOUSEINFO *mi);
+    void OnMiddleButtonUp(MOUSEINFO *mi);
+    void OnMiddleButtonDown(MOUSEINFO *mi);
+};
+
+void MouseRptParser::OnMouseMove(MOUSEINFO *mi)
+{
+  delta[0] = mi->dX;
+  delta[1] = mi->dY;
+};
+void MouseRptParser::OnLeftButtonUp	(MOUSEINFO *mi)
+{
+  lmb = 0;
+};
+void MouseRptParser::OnLeftButtonDown	(MOUSEINFO *mi)
+{
+  lmb = 1;
+};
+void MouseRptParser::OnRightButtonUp	(MOUSEINFO *mi)
+{
+  rmb = 0;
+};
+void MouseRptParser::OnRightButtonDown	(MOUSEINFO *mi)
+{
+  rmb = 1;
+};
+void MouseRptParser::OnMiddleButtonUp	(MOUSEINFO *mi)
+{
+  mmb = 0;
+};
+void MouseRptParser::OnMiddleButtonDown	(MOUSEINFO *mi)
+{
+  mmb = 1;
+};
+
+MouseRptParser MousePrs;
+
+void setup()
+{
+  Mouse.begin();
+  Serial.begin( 115200 );
+  Usb.Init();
+  HidComposite.SetReportParser(1, &MousePrs);
+  HidMouse.SetReportParser(0, &MousePrs);
 }
 
-void loop() {
+void loop()
+{
+  delta[0] = 0;
+  delta[1] = 0;
+  Usb.Task();
+  // Left Mouse
+  if (lmb == 0){
+    Mouse.release(MOUSE_LEFT);
+  } else if (lmb == 1){
+    Mouse.press(MOUSE_LEFT);
+  }
+  // Right Mouse
+  if (rmb == 0){
+    Mouse.release(MOUSE_RIGHT);
+  } else if (rmb == 1){
+    Mouse.press(MOUSE_RIGHT);
+  }
+  // Middle Mouse
+  if (mmb == 0){
+    Mouse.release(MOUSE_MIDDLE);
+  } else if (mmb == 1){
+    Mouse.press(MOUSE_MIDDLE);
+  }
+
   if (Serial.available() > 0)
   {
+    Serial.print("SUCCESS");
     // Read Data
     String data = Serial.readStringUntil('x');
 
     // Gets demarcation between deltaX and DeltaY
     int ohHiMarc = data.indexOf(':');
+    Serial.println(data);
 
     // DeltaX & DeltaY
     delta[0] = data.substring(0, ohHiMarc).toInt();
     delta[1] = data.substring(ohHiMarc + 1).toInt();
-    
-    handleX(delta[0]);
 
-    if (ohHiMarc != -1)
-    {
-      handleY(delta[1]);
-    }
+    handleX(delta[0]);
+    handleY(delta[1]);
+  } else{
+    Mouse.move(delta[0], delta[1]);
   }
 }
 
@@ -124,5 +209,4 @@ void handleY(int dy){
     // Move Remainder
     Mouse.move(0, remainder, 0);
   }
-  
-}
+};
